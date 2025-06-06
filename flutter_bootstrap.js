69986d224baf40d8f9122f83dadef7447efe1b15
Pedro Celis <pcelis19@gmail.com>
2025-06-06 15:00:25 -0700

    v0.5.0.14

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2714754472"
+    serviceWorkerVersion: "2543783043"
   }
 });
